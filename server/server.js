8ab32885ce12ef51149371f613e2e1019ed030ac
Your Name <vivianodekhiran@gmail.com>
2022-10-21 21:22:15 -0400

    committing to overwrite changes from dev branch

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+const cors = require('cors');
 
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) =>
